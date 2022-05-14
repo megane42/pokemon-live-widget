032dc69c51e20af234da36c4142fb8652fcf4df9
megane42 <h.kzm.gm@gmail.com>
2022-05-15 03:05:03 +0900

    import elm code from js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.css'
+import { Elm } from './src/Main.elm'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const root = document.querySelector("#app");
+const app = Elm.Main.init({ node: root })
