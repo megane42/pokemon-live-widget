5e7187abadb18c3338ed67bf361523bf9108b326
megane42 <h.kzm.gm@gmail.com>
2022-05-21 20:51:09 +0900

    testing fireStore

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 import './style.css'
 import { Elm } from './src/Main.elm'
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, getDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9YxMW-T75dZ-pm1QcxjZd6Jt_Yy82aVg",
+  authDomain: "pokemon-live-widget.firebaseapp.com",
+  projectId: "pokemon-live-widget",
+  storageBucket: "pokemon-live-widget.appspot.com",
+  messagingSenderId: "123902109283",
+  appId: "1:123902109283:web:4e95b1ab2100d3ad11fa56"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db          = getFirestore(firebaseApp);
+
+const battleTeamSelectionsCollectionRef = collection(db, "battleTeamSelections");
+const battleTeamSelectionsQuerySnapshot = await getDocs(battleTeamSelectionsCollectionRef);
+for (let battleTeamSelectionDocumentSnapshot of battleTeamSelectionsQuerySnapshot.docs) {
+  const teamSelectionDocumentRef      = battleTeamSelectionDocumentSnapshot.data().teamSelection;
+  const teamSelectionDocumentSnapshot = await getDoc(teamSelectionDocumentRef);
+  const pokemonDocumentRef            = teamSelectionDocumentSnapshot.data().pokemon;
+  const pokemonDocumentSnapshot       = await getDoc(pokemonDocumentRef);
+  console.log(pokemonDocumentSnapshot.data())
+};
 
 const root = document.querySelector("#app");
 const app = Elm.Main.init({ node: root })
